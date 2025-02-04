5086ff7b59d154f53182eb405bf9d4a1a38cc1a1
Javier Soria <javiersg1612@gmail.com>
2025-02-04 22:19:02 +0100

    feat: express validators (#10)

--- src/interface/reservations/express/update.controller.ts
+++ src/interface/reservations/express/update.controller.ts
@@ -3,8 +3,17 @@ import { container } from '@infra/shared/dependecyContainer';
 import { ReservationServiceError } from '@shared/errors';
 import { Request, Response } from 'express';
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { validationResult } = require('express-validator');
+
 export const updateReservationController = async (req: Request, res: Response): Promise<void> => {
   try {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+    }
+
     const { id } = req.params;
     const updatedReservation = {
       ...(req.body as UpdateReservationUsecaseInput),
